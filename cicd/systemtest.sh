a8b5d14eaa79a454dc5d2981f1f9a72e6b50af91
Tim Scott <tscott8706@gmail.com>
2017-10-28 11:50:28 -0500

    Package now installs with scripts, and system testing shows that the scripts can call into the run function

--- cicd/systemtest.sh
+++ cicd/systemtest.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
-echo "systemtest"
+jenkinsutils http://192.168.1.1:8080 init
+jenkinsutils http://192.168.1.1:8080 plugins download
+jenkinsutils http://192.168.1.1:8080 list
+jenkinsutils http://192.168.1.1:8080 create config
+jenkinsutils http://192.168.1.1:8080 jobstatus
+
--- setup.py
+++ setup.py
@@ -2,12 +2,17 @@ from distutils.core import setup
 
 package_name = "jenkinsutils"
 setup(
-    name=package_name,
-    version="1.0.0",
-    description="Some utilities for Jenkins using the jenkinsapi library",
-    author="Tim Scott",
-    author_email="tim@developerautomation.com",
-    requirements=["jenkinsapi"],
-    url="https://github.com/tscott8706/jenkinsutils",
-    packages=[package_name]
+    name = package_name,
+    version = "1.0.0",
+    description = "Some utilities for Jenkins using the jenkinsapi library",
+    author = "Tim Scott",
+    author_email = "tim@developerautomation.com",
+    requirements = ["jenkinsapi"],
+    url = "https://github.com/tscott8706/jenkinsutils",
+    packages = [package_name],
+    entry_points = {
+        "console_scripts": [
+            "jenkinsutils = jenkinsutils.run:run"
+        ]
+    },
 )
